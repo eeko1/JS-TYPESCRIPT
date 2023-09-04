c43cf862913cbdc305604bb9dad9764e92bb8f58
eeko1 <eemi.korhonen@metropolia.fi>
2023-09-04 15:42:04 +0300

    teht 3 valmis

---
+++ js-kertaus3/dom-assignments/t2/t2.js
@@ -0,0 +1,51 @@
+// array for todo list
+'use strict';
+
+const todoList = [
+  {
+    id: 1,
+    task: 'Learn HTML',
+    completed: true,
+  },
+  {
+    id: 2,
+    task: 'Learn CSS',
+    completed: true,
+  },
+  {
+    id: 3,
+    task: 'Learn JS',
+    completed: false,
+  },
+  {
+    id: 4,
+    task: 'Learn TypeScript',
+    completed: false,
+  },
+  {
+    id: 5,
+    task: 'Learn React',
+    completed: false,
+  },
+];
+
+const ul = document.querySelector('ul');
+
+for (const todo of todoList) {
+  const li = document.createElement('li');
+
+  const input = document.createElement('input');
+  input.type = 'checkbox';
+  input.id = todo.id;
+  if (todo.completed) {
+    input.checked = true;
+  }
+
+  const label = document.createElement('label');
+  label.htmlFor = todo.id;
+  label.textContent = todo.task;
+
+  li.appendChild(input);
+  li.appendChild(label);
+  ul.appendChild(li);
+}
